5567637889ee294e9715cc162638f89cd5eb837a
Kunal Umap <kunalgumap@gmail.com>
2024-05-11 19:27:04 +0530

    Mail Template Update

--- src/Mail/Mail.service.ts
+++ src/Mail/Mail.service.ts
@@ -1,22 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { Maildata } from './interfaces/maildata.interface';
 import { SESv2Client, SendEmailCommand } from '@aws-sdk/client-sesv2';
+import { RMOrderUpdateTemplate } from './templates/RMOrderUpdate';
+const config = require('config') 
+const { awsAccessKey, awsSecretAccessKey, awsReagion, awsSender} = config.get('awsSESconfig');
 
 @Injectable()
 export class MailService {
   private readonly sesClient: SESv2Client;
-
+  
   constructor() {
     this.sesClient = new SESv2Client({
-      region: process.env.AWS_SES_REGION,
+      region: awsReagion,
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: awsAccessKey,
+        secretAccessKey:awsSecretAccessKey,
       },
     });
   }
 
   async sendMail(maildata: Maildata): Promise<void> {
+    const htmlMail = RMOrderUpdateTemplate(maildata.datamain);
     const params = {
       Destination: {
         ToAddresses: [maildata.mail],
@@ -30,7 +34,7 @@ export class MailService {
             },
             Text: {
               Charset: 'UTF-8',
-              Data: maildata.data,
+              Data: `${htmlMail}`,
             },
           },
           Subject: {
@@ -39,7 +43,7 @@ export class MailService {
           },
         },
       },
-      FromEmailAddress: process.env.AWS_SES_SWNDER,
+      FromEmailAddress: awsSender,
     };
     try {
       await this.sesClient.send(new SendEmailCommand(params));
