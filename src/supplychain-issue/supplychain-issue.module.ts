497eedb8d9bafb81e86a933b6f2213081b9cd4d7
Prajna1999 <gituprajna20@gmail.com>
2024-06-01 22:01:41 +0530

    ADD:enum types for supplychainIssue

--- src/supplychain-issue/supplychain-issue.module.ts
+++ src/supplychain-issue/supplychain-issue.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SupplychainIssueService } from './supplychain-issue.service';
 import { SupplychainIssueController } from './supplychain-issue.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SupplyChainIssueCategory } from './entities/supplychain-issue-category.entity';
+import { SupplychainIssue } from './entities/supplychain-issue.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([SupplyChainIssueCategory, SupplychainIssue])],
   controllers: [SupplychainIssueController],
   providers: [SupplychainIssueService],
 })
